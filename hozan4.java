3c40ea6354f3a5557d67763081c24617e54ea8bb
Hozan9 <hozan.bahaden.93@gmail.com>
2023-03-09 15:20:19 +0100

    Hozan4

---
+++ hozan4.java
@@ -0,0 +1,8 @@
+public class hozan4 {
+    public class FeatureB {
+
+        public static void main(String[] args) {
+            System.out.println("Das ist Feature B von: hozan");
+        }
+    }
+}
